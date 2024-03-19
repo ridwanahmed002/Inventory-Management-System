3d92b73601219dc5976dc1d949853c6f33744c81
ridwanahmed002 <ridwanahmed.44504@gmail.com>
2024-03-19 09:58:05 +0600

    edit employee 4 files

--- Admin/model/db.php
+++ Admin/model/db.php
@@ -6,7 +6,6 @@ class db {
     private $password = "";
     private $dbname = "Admin";
 
-    // Open connection to the database
     function openConn() {
         $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
         if ($conn->connect_error) {
@@ -45,7 +44,7 @@ class db {
         return $conn->query($sqlstr);
     }
 
-    // Updated addEmployee function to include email
+    // addEmployee 
     function addEmployee($conn, $fname, $lname, $age, $gender, $email, $contact, $address, $section) {
         $sqlstr = "INSERT INTO employee (fname, lname, age, gender, email, contact, address, section) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($sqlstr);
@@ -55,56 +54,46 @@ class db {
         return $result;
     }
 
-
-    // Retrieve all employees data
     function getAllEmployees($conn) {
         $sqlstr = "SELECT * FROM employee";
         return $conn->query($sqlstr);
     }
 
-    // Update employee
-    function updateEmployee($conn, $id, $fname, $lname, $email, $section, $phone) {
-        $sqlstr = "UPDATE employee SET fname='$fname', lname='$lname', email='$email', section='$section', phone='$phone' WHERE id='$id'";
+    function updateEmployee($conn, $employee_id, $fname, $lname, $email, $section, $contact) {
+        $sqlstr = "UPDATE employee SET fname='$fname', lname='$lname', email='$email', section='$section', contact='$contact' WHERE employee_id='$employee_id'";
         return $conn->query($sqlstr);
     }
 
-    // Delete employee by ID
     function deleteEmployeeById($conn, $id) {
         $sqlstr = "DELETE FROM employee WHERE id = '$id'";
         return $conn->query($sqlstr);
     }
 
-    // Search employee by email
-    function searchEmployeeByEmail($conn, $email) {
-        $sqlstr = "SELECT * FROM employee WHERE email = '$email'";
+    function searchEmployeeById($conn, $employee_id) {
+        $sqlstr = "SELECT * FROM employee WHERE employee_id = '$employee_id'";
         return $conn->query($sqlstr);
     }
 
-    // Search employee by phone
     function searchEmployeeByPhone($conn, $phone) {
         $sqlstr = "SELECT * FROM employee WHERE phone = '$phone'";
         return $conn->query($sqlstr);
     }
 
-    // Add warehouse
     function addWarehouse($conn, $name, $location) {
         $sqlstr = "INSERT INTO warehouse (name, location) VALUES ('$name', '$location')";
         return $conn->query($sqlstr);
     }
 
-    // Remove warehouse by ID
     function removeWarehouseById($conn, $id) {
         $sqlstr = "DELETE FROM warehouse WHERE id = '$id'";
         return $conn->query($sqlstr);
     }
 
-    // Get customer support information by section
     function getCustomerSupportBySection($conn, $section) {
         $sqlstr = "SELECT email, phone FROM employee WHERE section = '$section'";
         return $conn->query($sqlstr);
     }
 
-    // Close connection to the database
     function closeConn($conn) {
         $conn->close();
     }
