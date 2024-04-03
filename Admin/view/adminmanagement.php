cc73f9734022ce6bfa85c1652c07fc12942b8f4b
ridwanahmed002 <ridwanahmed.44504@gmail.com>
2024-04-03 22:07:17 +0600

    admin management home style

--- Admin/view/adminmanagement.php
+++ Admin/view/adminmanagement.php
@@ -4,16 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Admin Management</title>
+    <link rel="stylesheet" href="../css/adminManagement.css">
 </head>
 
 <body>
-    <h2>Admin Management</h2>
-    <ul>
-        <li><a href="addadmin.php">Add Admin</a></li>
-        <li><a href="removeadmin.php">Remove Admin</a></li>
-        <li><a href="searchadmin.php">Search Admin</a></li>
-        <li><a href="listadmins.php">List Admins</a></li>
-    </ul>
+    <div class="admin-management-container">
+        <h2>Admin Management</h2>
+        <ul>
+            <li><a href="addadmin.php">Add Admin</a></li>
+            <li><a href="removeadmin.php">Remove Admin</a></li>
+            <li><a href="searchadmin.php">Search Admin</a></li>
+            <li><a href="listadmins.php">List Admins</a></li>
+        </ul>
+        <a href="adminhome.php" class="back-button">Back</a>
+    </div>
 </body>
 
 </html>
