a11c997269afcde476fc744b2b6e55c4a26d5d35
ridwanahmed002 <ridwanahmed.44504@gmail.com>
2024-04-03 21:15:07 +0600

    admin dashboard

--- Admin/view/adminhome.php
+++ Admin/view/adminhome.php
@@ -5,15 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/adminHome.css">
 </head>
 
 <body>
-    <h2>Admin Dashboard</h2>
-    <ul>
-        <li><a href="adminmanagement.php">Admin Management</a></li>
-        <li><a href="warehousemanagement.php">Warehouse Management</a></li>
-        <li><a href="employeemanagement.php">Employee Management</a></li>
-    </ul>
+    <div class="dashboard-container">
+        <h2>Admin Dashboard</h2>
+        <ul>
+            <li><a href="adminmanagement.php">Admin Management</a></li>
+            <li><a href="warehousemanagement.php">Warehouse Management</a></li>
+            <li><a href="employeemanagement.php">Employee Management</a></li>
+        </ul>
+        <a href="adminlogin.php" class="back-button">Back</a>
+    </div>
 </body>
 
 </html>
