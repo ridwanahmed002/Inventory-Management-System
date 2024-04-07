4cf61ee61c086b83204bb32ec9cd7edcd0bce375
ridwanahmed002 <ridwanahmed.44504@gmail.com>
2024-04-07 15:11:06 +0600

    ..

--- Admin/view/employeemanagement.php
+++ Admin/view/employeemanagement.php
@@ -4,16 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Employee Management</title>
+    <link rel="stylesheet" href="../css/employeeManagement.css">
 </head>
 
 <body>
-    <h2>Employee Management</h2>
-    <ul>
-        <li><a href="addemployee.php">Add Employee</a></li>
-        <li><a href="removeemployee.php">Remove Employee</a></li>
-        <li><a href="searchemployee.php">Search Employee</a></li>
-        <li><a href="listemployees.php">List Employees</a></li>
-    </ul>
+    <div class="employee-management-container">
+        <h2>Employee Management</h2>
+        <ul>
+            <li><a href="addemployee.php">Add Employee</a></li>
+            <li><a href="removeemployee.php">Remove Employee</a></li>
+            <li><a href="searchemployee.php">Search Employee</a></li>
+            <li><a href="listemployees.php">List Employees</a></li>
+            <li><a href="editemployees.php">Edit Employees</a></li>
+        </ul>
+        <a href="adminhome.php" class="back-button">Back</a>
 </body>
 
 </html>
